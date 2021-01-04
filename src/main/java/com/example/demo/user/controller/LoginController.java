dee930f1ebce888b535a28a6de30f59c06b97e91
horobito <odkfu1@gmail.com>
2021-01-04 19:23:20 +0900

    spring server

--- src/main/java/com/example/demo/user/controller/LoginController.java
+++ src/main/java/com/example/demo/user/controller/LoginController.java
@@ -5,7 +5,6 @@ import com.example.demo.user.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
