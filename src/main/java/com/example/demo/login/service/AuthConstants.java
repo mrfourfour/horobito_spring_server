98dc37912a990f63ccd796da8064b11ee862f094
horobito <odkfu1@gmail.com>
2021-01-18 21:16:13 +0900

    반쪽짜리...보다 더 한 1/4짜리...구현..

--- src/main/java/com/example/demo/login/service/AuthConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.login.service;
-
-public final class AuthConstants {////
-
-    public static final String AUTH_HEADER = "Authorization";
-    public static final String TOKEN_TYPE = "BEARER";
-}
--- src/main/java/com/example/demo/login/service/InputNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.login.service;
-
-public class InputNotFoundException extends RuntimeException {
-
-    public InputNotFoundException(){
-        super();
-    }
-}
