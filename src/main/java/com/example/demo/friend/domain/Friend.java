3c65639b12dec23e3b519e05069ec41040776a47
jhkim <62026666+horobito@users.noreply.github.com>
2021-03-17 00:37:56 +0900

    기존 오류 제거 및 friend 도메인 영역 추가

--- src/main/java/com/example/demo/friend/domain/Friend.java
+++ src/main/java/com/example/demo/friend/domain/Friend.java
@@ -1,7 +1,8 @@
 package com.example.demo.friend.domain;
 
 
-import com.example.demo.user.domain.User;
+import com.example.demo.friend.service.FriendDto;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,29 +10,29 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "friend")
-@Getter
 @Setter
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Friend {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friend_pk")
     private Long id;
 
-    private Long frinedId;
+    @Embedded
+    private FriendId friendId;
 
     @Embedded
-    private Owner owner;
+    private FriendName friendname;
 
-    private String friendname;
-
-
-
-
-    public Friend(User friendUser){
-        this.frinedId = friendUser.getId();
-        this.friendname = friendUser.getUserId();
+    public Friend(FriendId friendId, FriendName friendname){
+        this.friendId = friendId;
+        this.friendname = friendname;
     }
 
+    public static Friend createFriend(FriendId friendId, FriendName friendname) {
+        return new Friend(friendId, friendname);
 
+    }
 }
