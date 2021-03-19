c84502b1a7fae8a7eb5cda878e5ab2206bef151d
jhkim <62026666+horobito@users.noreply.github.com>
2021-03-19 21:19:54 +0900

    *변경내역 - 1)  반환형을 page로 변경
    * 이유
    - Pageable를 사용하니 반환형도 그것에 알맞게 변경

--- src/main/java/com/example/demo/friend/domain/FriendShipRepository.java
+++ src/main/java/com/example/demo/friend/domain/FriendShipRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.friend.domain;
 
 import com.example.demo.user.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,7 +11,7 @@ import java.util.Optional;
 
 public interface FriendShipRepository  extends JpaRepository<Friendship, Long> {
 
-    List<Friendship> findAllByUser(User user, Pageable pageable);
+    Page<Friendship> findAllByUser(User user, Pageable pageable);
 
     List<Friendship> findAllByUser(User user);
 
