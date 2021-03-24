15ff4ca543cfecc023f22ff17e1748267dd752c3
jhkim <62026666+horobito@users.noreply.github.com>
2021-03-24 22:59:36 +0900

    * User 를 대체할 새로운 도메인 UserInfo 완성

--- src/main/java/com/example/demo/friend/domain/UserName.java
+++ src/main/java/com/example/demo/friend/domain/UserName.java
@@ -1,4 +1,25 @@
 package com.example.demo.friend.domain;
 
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserName {
+
+    @Column(name = "user_name")
+    private String username;
+
+
+    private UserName(String username){
+        this.username = username;
+    }
+
+    public static UserName create(String userName){
+        return new UserName(userName);
+    }
 }
