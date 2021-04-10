7100fcde23cea744acbe442bd33939822f9e95f9
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-10 19:41:21 +0900

    * repository 생성

--- src/main/java/com/example/demo/preperredPerson/service/PreferredPersonService.java
+++ src/main/java/com/example/demo/preperredPerson/service/PreferredPersonService.java
@@ -1,4 +1,17 @@
 package com.example.demo.preperredPerson.service;
 
+
+import com.example.demo.feed.domain.FeedRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PreferredPersonService {
+
+    private final FeedRepository feedRepository;
+
+
+
+
 }
