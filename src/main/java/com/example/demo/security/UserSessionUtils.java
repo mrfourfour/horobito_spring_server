74156427b3a81e354a0242945d4a99e21a03a865
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-10 18:01:28 +0900

    * 로그인된 User 정보 가져오기 위한 서비스 형성   -> 수업자료 사용해서 적용

---
+++ src/main/java/com/example/demo/security/UserSessionUtils.java
@@ -0,0 +1,18 @@
+package com.example.demo.security;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.SecurityUser;
+
+import java.nio.file.AccessDeniedException;
+
+public class UserSessionUtils {
+
+    public static SecurityUser getSessionUser() throws AccessDeniedException {
+        Object detais = SecurityContextHolder.getContext().getAuthentication().getDetails();
+
+        if (!(detais instanceof SecurityUser)){
+            throw new AccessDeniedException("유저 정보가 없습니다.");
+        }
+        return (SecurityUser) detais;
+    }
+}
---
+++ src/main/java/com/example/demo/user/service/UserSessionService.java
@@ -0,0 +1,26 @@
+package com.example.demo.user.service;
+
+
+import com.example.demo.security.UserSessionUtils;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.SecurityUser;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.AccessDeniedException;
+
+@Service
+@RequiredArgsConstructor
+public class UserSessionService {
+
+    private final UserRepository userRepository;
+
+
+    public User getLoginedUser() throws AccessDeniedException {
+        SecurityUser securityUser = UserSessionUtils.getSessionUser();
+        Long userId = securityUser.getId();
+
+        return userRepository.findUserById(userId);
+    }
+}
