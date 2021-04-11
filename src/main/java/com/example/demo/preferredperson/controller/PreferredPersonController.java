34da7f5bdad33a5d4bc6bcd5c4a4bc77548b7f4f
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-11 23:17:55 +0900

    * PreferredPersonController Feed 부분 구현

--- src/main/java/com/example/demo/preferredperson/controller/PreferredPersonController.java
+++ src/main/java/com/example/demo/preferredperson/controller/PreferredPersonController.java
@@ -1,7 +1,10 @@
 package com.example.demo.preferredperson.controller;
 
+import com.example.demo.preferredperson.service.PreferenceResult;
 import com.example.demo.preferredperson.service.PreferredPersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,19 @@ public class PreferredPersonController {
 
     @PostMapping("/feeds/{feedId}/likes")
     public void likeFeedByFeedId(@PathVariable Long feedId) throws AccessDeniedException {
-        preferredPersonService.likeFeedByFeedId(feedId);
+        PreferenceResult result = preferredPersonService.likeFeedByFeedId(feedId);
+
+        switch (result){
+            case SUCCESS:
+                ResponseEntity.ok();
+                break;
+            case MY_FEED_ERROR:
+            case FEED_NOT_FOUND:
+                ResponseEntity.status(HttpStatus.BAD_REQUEST);
+                break;
+            case NOT_MY_FRIEND:
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED);
+        }
+
     }
 }
