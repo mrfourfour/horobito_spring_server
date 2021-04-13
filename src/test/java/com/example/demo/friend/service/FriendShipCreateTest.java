f668332def8b012ba7774324b29ea05a5e9cd20d
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-14 00:30:25 +0900

    * Create 중 수락하는 경우에 대한 테스트 코드

--- src/test/java/com/example/demo/friend/service/FriendShipCreateTest.java
+++ src/test/java/com/example/demo/friend/service/FriendShipCreateTest.java
@@ -120,5 +120,61 @@ class FriendShipCreateTest {
 
     }
 
+    @DisplayName("친구관계 맺기 테스트 2. 이미 신청 또는 수락된 경우 ")
+    @Test
+    void testForAccept() throws AccessDeniedException {
+
+        FriendShipService friendShipService
+                = new FriendShipService(friendShipRepository,
+                userSessionService, userService);
+
+        //given
+
+        String[] friender = { "1", "jihwan"};
+        String [] friendee = { "2", "friendee"};
+
+        PersonId myId = PersonId.create(Long.parseLong(friender[0]) );
+        PersonId friendId = PersonId.create(Long.parseLong(friendee[0]));
+
+        PersonName myName = PersonName.create((String) friender[1]);
+        PersonName friendName = PersonName.create((String) friendee[1]);
+
+        Friender frienderMe = Friender.create(myId, myName);
+        Friendee friendeeYou = Friendee.create(friendId, friendName);
+
+        Friender frienderYou = Friender.create(friendId, friendName);
+        Friendee friendeeMe = Friendee.create(myId, myName);
+
+        Friendship forwardFriendShip
+                = Friendship.create(frienderMe, friendeeYou);
+
+        Friendship backwardFriendShip
+                = Friendship.create(frienderYou, friendeeMe);
+        backwardFriendShip.acceptFriendShip();
+
+
+        //when
+        when(userService.findUserInfo())
+                .thenReturn(friender);
+
+        when(userService.findUserInfo(any()))
+                .thenReturn(friendee);
+
+        when(friendShipRepository
+                .findFriendshipByFrienderAndFriendee_FriendeeId(
+                        any(), any()
+                )).thenReturn(forwardFriendShip)
+                .thenReturn(forwardFriendShip)
+                .thenReturn(backwardFriendShip);
+
+
+
+        //then
+        FriendShipResult result = friendShipService.create(1L);
+        System.out.println(result);
+        assertEquals(FriendShipResult.SUCCESS, result);
+
+    }
+
 
 }
