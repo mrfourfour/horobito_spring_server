d635104cb1dc93c77d2004ccb9083719e4b0a8fe
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-15 00:40:04 +0900

    * Controller 층에서 삭제요청에 대한 결과 값에 따른 응답 코드 설정 완료

--- src/main/java/com/example/demo/feed/controller/FeedController.java
+++ src/main/java/com/example/demo/feed/controller/FeedController.java
@@ -2,9 +2,12 @@ package com.example.demo.feed.controller;
 
 
 import com.example.demo.feed.domain.Feed;
+import com.example.demo.feed.domain.RequestResult;
 import com.example.demo.feed.service.FeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
@@ -35,8 +38,22 @@ public class FeedController {
     }
 
     @DeleteMapping("/feeds/{feedId}")
-    public void deleteFeedByFeedId(@PathVariable Long feedId){
-        feedService.deleteFeedByFeedId(feedId);
+    public void deleteFeedByFeedId(@PathVariable Long feedId) throws AccessDeniedException {
+        RequestResult result = feedService.deleteFeedByFeedId(feedId);
+
+        switch (result){
+            case OK:
+                ResponseEntity.ok();
+                break;
+            case BAD_REQUEST:
+                ResponseEntity.status(HttpStatus.BAD_REQUEST);
+                break;
+            case FORBIDDEN:
+                ResponseEntity.status(HttpStatus.FORBIDDEN);
+                break;
+            case UNAUTHORIZED:
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED);
+        }
 
     }
 
