e1ce8d32c2e71c82f0d03d9072e91ac5c851ab6a
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-15 23:04:23 +0900

    * UserDetailsService 구현 및 그에 필요한 코드들 작성중 (2)

--- src/main/java/com/example/demo/security/service/UserDetailsService.java
+++ src/main/java/com/example/demo/security/service/UserDetailsService.java
@@ -1,15 +1,19 @@
 package com.example.demo.security.service;
 
 
+import com.example.demo.user.domain.Authority;
 import com.example.demo.user.domain.UserRepository;
 import com.example.demo.user.domain.Username;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.SecurityUser;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,8 +30,14 @@ public class UserDetailsService implements org.springframework.security.core.use
                 .map(user -> new SecurityUser(
                         user.getId(),
                         user.getUserBasicInfo().getUsername(),
-                        user.g
-                ))
-        return null;
+                        user.getUserBasicInfo().getPassword(),
+                        user.getAuthorities()
+                                .stream()
+                                .map(Authority::getAuthority)
+                                .map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toSet())
+                ));
+        return user1.orElseThrow(
+                ()-> new UsernameNotFoundException("Can't find by your username"));
     }
 }
