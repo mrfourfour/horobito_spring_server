f92e55892219955d46ba0c100d0236ebe04fdd07
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-15 23:25:20 +0900

    * UserAuthenticationFilter 에 Object Mapper 넣어줌

--- src/main/java/com/example/demo/security/infrastructure/UserAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/infrastructure/UserAuthenticationFilter.java
@@ -6,14 +6,30 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@Component
+
+
 public class UserAuthenticationFilter extends BasicAuthenticationFilter {
+
+    private final ObjectMapper objectMapper;
+
     public UserAuthenticationFilter(AuthenticationManager authenticationManager, ObjectMapper objectMapper) {
         super(authenticationManager);
+        this.objectMapper = objectMapper;
     }
 
-    public UserAuthenticationFilter(AuthenticationManager authenticationManager, AuthenticationEntryPoint authenticationEntryPoint) {
+    public UserAuthenticationFilter(AuthenticationManager authenticationManager, AuthenticationEntryPoint authenticationEntryPoint,  ObjectMapper objectMapper) {
         super(authenticationManager, authenticationEntryPoint);
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        super.doFilterInternal(request, response, chain);
     }
 }
