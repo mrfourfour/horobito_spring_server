427b6d904f1ecd5f640ce920e304ad4257692f02
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-20 17:48:27 +0900

    * [친구요청 확인 테스트] 1. 알맞은 요청 확인 테스트

--- src/test/java/com/example/demo/friend/service/FriendShipFindTest.java
+++ src/test/java/com/example/demo/friend/service/FriendShipFindTest.java
@@ -3,25 +3,21 @@ package com.example.demo.friend.service;
 
 import com.example.demo.friend.domain.*;
 import com.example.demo.user.service.UserService;
-import com.example.demo.user.service.UserSessionService;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-
 import java.nio.file.AccessDeniedException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.stream.Stream;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 public class FriendShipFindTest {
 
@@ -30,23 +26,25 @@ public class FriendShipFindTest {
 
 
 
+
     @Mock
     UserService userService;
 
-    @DisplayName("친구관계 요청 테스트  ")
+    @DisplayName("친구관계 요청 확인 테스트1. 알맞은 요청 테스트  ")
+
     @Test
     void testForAccept() throws AccessDeniedException {
 
-        FriendShipService friendShipService
+        FriendShipService sut
                 = new FriendShipService(friendShipRepository,
 userService);
 
         //given
 
-        String[] person1Info = { "1", "friender1"};
-        String [] person2Info = { "2", "friender2"};
-        String [] person3Info = {"3", "friendee1"};
-        String [] person4Info = {"4", "friendee2"};
+        String[] person1Info = { "1", "person1"};
+        String [] person2Info = { "2", "person2"};
+        String [] person3Info = {"3", "person3"};
+        String [] person4Info = {"4", "person4"};
 
         PersonId person1Id = PersonId.create(Long.parseLong(person1Info[0]) );
         PersonId person2Id = PersonId.create(Long.parseLong( person2Info[0]));
@@ -68,47 +66,39 @@ userService);
         Friendee friendee3 = Friendee.create(person3Id, person3Name);
         Friendee friendee4 = Friendee.create(person4Id, person4Name);
 
-        Friendship friendship1 = Friendship.create(friender4, friendee1);
-        Friendship friendship2 = Friendship.create(friender3, friendee2);
-        Friendship friendship3 = Friendship.create(friender2, friendee2);
-        Friendship friendship4 = Friendship.create(friender1, friendee3);
+        Friendship forwardFriendship1 = Friendship.create(friender4, friendee1);
+        forwardFriendship1.requestFriendShip();
+        Friendship forwardFriendship2 = Friendship.create(friender3, friendee1);
+        forwardFriendship2.requestFriendShip();
+        Friendship forwardFriendship3 = Friendship.create(friender2, friendee1);
+        forwardFriendship3.requestFriendShip();
+        Friendship forwardFriendship4 = Friendship.create(friender1, friendee3);
+        forwardFriendship4.requestFriendShip();
 
+        List<Friendship> forwardFriendshipList = new ArrayList<>();
 
+        forwardFriendshipList.add(forwardFriendship1);
+        forwardFriendshipList.add(forwardFriendship2);
+        forwardFriendshipList.add(forwardFriendship3);
 
+        Page<Friendship> forwardPage =  new PageImpl<>(forwardFriendshipList);
 
         //when
         // 4,3,2 는 1에 친구 요청
         // 1은 3만 친구 요청
+        when(userService.findUserInfo()).thenReturn(person1Info);
+        when(friendShipRepository
+                .findAllByFriendee_FriendeeId(any(), any()))
+                .thenReturn(forwardPage);
 
-        when(userService.findUserInfo())
-                .thenReturn(person1Info);
-
-        friendShipRepository.save(friendship1);
-        friendShipRepository.save(friendship2);
-        friendShipRepository.save(friendship3);
-        friendShipRepository.save(friendship4);
-
-        Page<Friendship> dtos = friendShipRepository
-                .findAllByFriendee_FriendeeId(
-                       person1Id, PageRequest.of(0, 4));
-
-
-        System.out.println();
 
         //then
-        List<FriendDto> result = friendShipService.findRequestForMe(0, 4);
-        for (FriendDto dtoss : result){
-            System.out.println(dtos);
+
+        List<FriendDto> result = sut.findRequestForMe(0, 4);
+        for (FriendDto dto : result){
+            System.out.println(dto);
         }
 
-
-    }
-
-    private FriendDto toFriendDto(BasicInfo basicInfo) {
-        return new FriendDto(
-                basicInfo.getId(),
-                basicInfo.getName()
-        );
     }
 
 }
