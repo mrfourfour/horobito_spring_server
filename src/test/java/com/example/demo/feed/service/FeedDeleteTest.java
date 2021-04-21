11aae7296cc3ec8176751ab67f36c5cb5b975ebe
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-21 18:39:43 +0900

    * [Feed 삭제하기] 2. <타인의 글에 삭제를 요청했을 경우> 테스트

--- src/test/java/com/example/demo/feed/service/FeedDeleteTest.java
+++ src/test/java/com/example/demo/feed/service/FeedDeleteTest.java
@@ -1,7 +1,7 @@
 package com.example.demo.feed.service;
 
 
-import com.example.demo.feed.domain.FeedRepository;
+import com.example.demo.feed.domain.*;
 import com.example.demo.friend.domain.FriendShipRepository;
 import com.example.demo.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.nio.file.AccessDeniedException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,6 +48,47 @@ public class FeedDeleteTest {
                 .thenReturn(person1);
 
         assertThrows(IllegalArgumentException.class,
+                ()->sut.deleteFeedByFeedId(1L));
+
+
+    }
+
+    @DisplayName("게시글 삭제 2.타인의 글에 삭제를 시도하였을 경우 ")
+    @Test
+    void test2() throws AccessDeniedException {
+        FeedService sut
+                = new FeedService(
+                feedRepository,
+                friendShipRepository,
+                userService
+        );
+
+        String[] userInfo = {"1", "hello"};
+
+        Long friendId = Long.parseLong("3");
+
+        Long feedId = Long.parseLong("2");
+
+
+
+        WriterId writerId = WriterId.create(friendId);
+        WriterName writerName = WriterName.create("writerName");
+
+        Writer writer = Writer.create(writerId, writerName);
+
+        Content content = Content.create("content");
+
+        Title title = Title.create("This is title");
+
+        Feed feed = FeedHelper.create(feedId, writer, title,  content);
+
+        when(userService.findUserInfo())
+                .thenReturn(userInfo);
+
+        when(feedRepository.findFeedByIdAndDeleted(any(), any()))
+                .thenReturn(feed);
+
+        assertThrows(IllegalAccessException.class,
                 ()->sut.deleteFeedByFeedId(1L));
 
 
