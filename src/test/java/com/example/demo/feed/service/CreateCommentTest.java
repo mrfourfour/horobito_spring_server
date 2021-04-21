1625a27ee52690ffbffe8990c4c1af7fecad6dad
jhkim <62026666+horobito@users.noreply.github.com>
2021-04-21 19:43:02 +0900

    * [comment 만들기] 4. 작성자와 친구가 승인되지 않은 경우

--- src/test/java/com/example/demo/feed/service/CreateCommentTest.java
+++ src/test/java/com/example/demo/feed/service/CreateCommentTest.java
@@ -122,4 +122,54 @@ public class CreateCommentTest {
                 sut.makeCommentByFeedIdAndContents(commentId, commentContent));
     }
 
+    @DisplayName("댓글 생성 4. feed 작성자와 친구가 아닐 경우 2) 친구관계가 아닌 경우 " )
+    @Test
+    void test4() throws AccessDeniedException {
+        CommentService sut
+                = new CommentService(feedRepository, userService, friendShipRepository);
+
+        Long commentId = Long.parseLong("3");
+        String commentContent = "asdf";
+
+        String[] person1Info = {"1", "hello"};
+
+        Long friendId = Long.parseLong("3");
+        String friendName = "hhhh";
+        Long feedId = Long.parseLong("2");
+
+        WriterId writerId = WriterId.create(friendId);
+        WriterName writerName = WriterName.create("writerName");
+        Writer writer = Writer.create(writerId, writerName);
+
+        Content content = Content.create("content");
+        Title title = Title.create("This is title");
+        Feed feed = FeedHelper.create(feedId, writer, title,  content);
+
+        PersonId person1Id = PersonId.create(Long.parseLong(person1Info[0]));
+        PersonId person2Id = PersonId.create(friendId);
+
+        PersonName person1Name = PersonName.create( person1Info[1]);
+        PersonName person2Name = PersonName.create( friendName);
+
+        Friender friender = Friender.create(person1Id, person1Name);
+        Friendee friendee = Friendee.create(person2Id, person2Name);
+
+        Friendship friendship = Friendship.create(friender, friendee);
+        friendship.requestFriendShip();
+
+        when(feedRepository.findFeedByIdAndDeleted(any(), any()))
+                .thenReturn(feed);
+
+        when(userService.findUserInfo())
+                .thenReturn(person1Info);
+
+        when(friendShipRepository
+                .findFriendshipByFriender_FrienderIdAndFriendee_FriendeeId(
+                        any(),
+                        any())).thenReturn(friendship);
+
+        assertThrows(IllegalAccessException.class, ()->
+                sut.makeCommentByFeedIdAndContents(commentId, commentContent));
+    }
+
 }
